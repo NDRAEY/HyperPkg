a995f0dbbeb1f8eec03a9fd681c31a4f42097dfe
NDRAEY <andrejpavlenko666@gmail.com>
2022-08-31 11:10:05 +0900

    Fixes

--- runtime/runtime.c
+++ runtime/runtime.c
@@ -1,6 +1,10 @@
+#define _XOPEN_SOURCE 500
+
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
 #include <time.h>
 #include <unistd.h>
 #include <errno.h>
