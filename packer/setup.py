2b1686d2ff615ff5658b58be21da45a0c59adb3a
NDRAEY <andrejpavlenko666@gmail.com>
2022-08-31 14:35:52 +0900

    fixes

--- packer/setup.py
+++ packer/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='hyperpkg',
-    version='0.0.1',
+    version='0.0.2',
     author="NDRAEY",
     description="HyperPkg utilities",
     scripts=["hyperpkg/hyperpkg-packer"],
